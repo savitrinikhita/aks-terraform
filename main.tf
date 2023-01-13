88818e2823d7c043bbbf838db96b9e1c015673e1
savitrinikhita <48665144+savitrinikhita@users.noreply.github.com>
2023-01-13 19:42:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,35 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "eastus"
+  name     = "example-aks"
+  location = "eastus"
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
 
-  identity {
-    type = "SystemAssigned"
-  }
+  identity {
+    type = "SystemAssigned"
+  }
 
-  tags = {
-    Environment = "Production"
-  }
+  tags = {
+    Environment = "Production"
+  }
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-  sensitive = true
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-  sensitive = true
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  sensitive = true
 }
-
